017cffa05cefb0ac9b1377778d4b7334730fa758
MarieLePanda <girardin.lucas@gmail.com>
2014-04-05 02:59:20 +0200

    Development Tree IHM
    - Add tree + scroll
    - Add default metamodel

--- src/IHM/MainWindow.java
+++ src/IHM/MainWindow.java
@@ -1,26 +1,77 @@
 package IHM;
+import java.awt.*;
 import javax.swing.*;
 import javax.swing.tree.*;
 
 public class MainWindow extends JFrame {
 
-	private JTree menu;
+	public JTree tree;
+        public JScrollPane scrollTree;
+        public JPanel panelTree;
+        public BorderLayout layoutTree; 
+        public Container containerTree;
+
 	public MainWindow(){
 
-		super("Title");
-		
-		DefaultMutableTreeNode rootFolder = new DefaultMutableTreeNode("Metamodel");
-		DefaultMutableTreeNode activityFolder = new DefaultMutableTreeNode("Activity");
-		rootFolder.add(activityFolder);
-		DefaultMutableTreeNode processFolder = new DefaultMutableTreeNode("Process");
-		rootFolder.add(processFolder);
-		DefaultMutableTreeNode functionFolder = new DefaultMutableTreeNode("Function");
-		rootFolder.add(functionFolder);
-		
-		menu = new JTree(rootFolder);
-		
-		this.add(menu);
-		
-		pack();		
+            super("Title");
+            initJtree();
+            
+            pack();		
 	}
+        
+        
+        public void initJtree(){
+            //containerTree = this.getContentPane();
+            //cont.
+            
+            //Metamodel
+            DefaultMutableTreeNode rootFolder = new DefaultMutableTreeNode("Metamodel");
+            
+            //Functionnal view
+            DefaultMutableTreeNode functionalView = new DefaultMutableTreeNode("Functional view");
+            rootFolder.add(functionalView);
+            DefaultMutableTreeNode activityFolder = new DefaultMutableTreeNode("Activity");
+            functionalView.add(activityFolder);
+            DefaultMutableTreeNode processFolder = new DefaultMutableTreeNode("Process");
+            functionalView.add(processFolder);
+            DefaultMutableTreeNode functionFolder = new DefaultMutableTreeNode("Function");
+            functionalView.add(functionFolder);
+            
+            //Application view
+            DefaultMutableTreeNode applicationView = new DefaultMutableTreeNode("Application view");
+            rootFolder.add(applicationView);
+            DefaultMutableTreeNode applicationFolder = new DefaultMutableTreeNode("Application");
+            applicationView.add(applicationFolder); 
+            DefaultMutableTreeNode interfaceFolder = new DefaultMutableTreeNode("Interface");
+            applicationView.add(interfaceFolder);
+            
+            //Technical view
+            DefaultMutableTreeNode technicalView = new DefaultMutableTreeNode("Technical view");
+            rootFolder.add(technicalView);
+            DefaultMutableTreeNode serverFolder = new DefaultMutableTreeNode("Server");
+            technicalView.add(serverFolder); 
+            DefaultMutableTreeNode databaseFolder = new DefaultMutableTreeNode("Database");
+            technicalView.add(databaseFolder);
+            DefaultMutableTreeNode technologieFolder = new DefaultMutableTreeNode("Technologie");
+            technicalView.add(technologieFolder);
+            
+            //assignment
+            tree = new JTree(rootFolder);
+            scrollTree = new JScrollPane(tree);
+            panelTree = new JPanel();
+            layoutTree = new BorderLayout();
+            panelTree.add(scrollTree, BorderLayout.WEST);
+            this.getContentPane().add(panelTree, BorderLayout.WEST);
+            //containerTree.add(panelTree);
+            //this.getContentPane().add(new JButton("WEST"), BorderLayout.WEST);
+
+
+            this.add(panelTree);
+          
+        }
+        
+        public void initFolder(){
+            
+            
+        }
 }
--- src/Launch/Launcher.java
+++ src/Launch/Launcher.java
@@ -1,12 +1,16 @@
 package Launch;
 import IHM.*;
+import javax.swing.WindowConstants;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MainWindow win = new MainWindow();
-		win.setVisible(true);
+            
+    // TODO Auto-generated method stub
+            MainWindow win = new MainWindow();
+            win.setVisible(true);
+            win.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
 	}
 
 }
