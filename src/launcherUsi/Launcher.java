6396865fedcc463b1ae449c7826176b9cb0d2f5c
MarieLePanda <girardin.lucas@gmail.com>
2014-06-14 17:11:11 +0200

    management plugin begining

--- src/launcherUsi/Launcher.java
+++ src/launcherUsi/Launcher.java
@@ -1,13 +1,19 @@
 package launcherUsi;
 import IHM.*;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import javax.swing.*;
 import myObject.User;
+import plugin.IModule;
+import plugin.ModuleLoader;
 
 public class Launcher {
 
     public static User userConnected = null;
 	public static void main(String[] args) {
-            
+            ModuleLoader.loadPlugin();
     // TODO Auto-generated method stub
             
             Runnable r = new Runnable(){
@@ -26,7 +32,12 @@ public class Launcher {
 		}
             };
         
-            SwingUtilities.invokeLater(r);
-	}
+            
+        SwingUtilities.invokeLater(r);
+        
+        
+    }
+        
+
 
 }
--- src/plugin/ModuleLoader.java
+++ src/plugin/ModuleLoader.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarFile;
@@ -18,52 +19,20 @@ import java.util.jar.Manifest;
  * @author lug13995
  */
 public class ModuleLoader { 
-	private static List<URL> urls = new ArrayList<URL>(); 
+    public static void loadPlugin(){
+        URLClassLoader systemClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+        try{
+           URL urlList[] = {new File("C:\\Users\\lug13995\\Documents\\GitHub\\UsiImportModule\\dist\\UsiImportModule.jar").toURL()};
+            ClassLoader loader = new URLClassLoader(urlList); 
+            String className = "usiimportmodule.PluginTest"; 
  
-	private static List<String> getModuleClasses(){ 
-		List<String> classes = new ArrayList<String>(); 
- 
-		//On liste les fichiers de module  
-		File[] files = new File("dossier").listFiles(new ModuleFilter()); 
- 
-		for(File f : files){ 
-			JarFile jarFile = null; 
- 
-			try { 
-				//On ouvre le fichier JAR 
-				jarFile = new JarFile(f); 
- 
-				//On récupère le manifest 
-				Manifest manifest = jarFile.getManifest(); 
- 
-				//On récupère la classe 
-				String moduleClassName = manifest.getMainAttributes().getValue("Module-Class"); 
- 
-				classes.add(moduleClassName); 
- 
-				urls.add(f.toURI().toURL()); 
-			} catch (IOException e) { 
-				e.printStackTrace(); 
-			} finally { 
-				if(jarFile != null){ 
-					try { 
-						jarFile.close(); 
-					} catch (IOException e) { 
-						e.printStackTrace(); 
-					} 
-				} 
-			} 
-		} 
- 
-		return classes; 
-	} 
- 
-	private static class ModuleFilter implements FileFilter { 
-		@Override 
-		public boolean accept(File file) { 
-			return file.isFile() && file.getName().toLowerCase().endsWith(".jar"); 
-		} 
-	}
+            IModule o = (IModule) Class.forName(className,true,loader).newInstance();
+            o.plug();
+        }
+        catch(Exception e){
+           e.printStackTrace();
+        }
+    }
         
         
 }
